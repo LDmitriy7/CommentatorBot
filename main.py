a54d1b0a031dbef7ac95cd846a4a45145e7c5574
levch <levchenko.d.a1998@gmail.com>
2022-03-05 13:36:16 +0300

    v 1.0.1

--- main.py
+++ main.py
@@ -3,36 +3,35 @@ import logging
 
 from pyrogram import types
 
-import api
 from config import SEND_AS_CHAT_ID
 from loader import bot, log
 
-TARGET_CHAT_IDS = set()
+
+# TARGET_CHAT_IDS = set()
 
 
-@api.after_connection
-async def on_startup():
-    async for dialog in bot.iter_dialogs():
-        chat = dialog.chat
-
-        if chat.type in ["group", "supergroup"]:
-            try:
-                res = await bot.set_send_as_chat(chat.id, SEND_AS_CHAT_ID)
-            except Exception as e:
-                log.exception(e)
-            else:
-                if res:
-                    TARGET_CHAT_IDS.add(chat.id)
-
-    log.warning(f'{TARGET_CHAT_IDS=}')
+# @api.after_connection
+# async def on_startup():
+#     async for dialog in bot.iter_dialogs():
+#         chat = dialog.chat
+#
+#             try:
+#                 res = await bot.set_send_as_chat(chat.id, SEND_AS_CHAT_ID)
+#             except Exception as e:
+#                 log.exception(e)
+#             else:
+#                 if res:
+#                     TARGET_CHAT_IDS.add(chat.id)
+#
+#     log.warning(f'{TARGET_CHAT_IDS=}')
 
 
 @bot.on_message()
 async def send_comment(_, msg: types.Message):
-    if not msg.forward_from_chat:
+    if msg.chat.type not in ["group", "supergroup"]:
         return
 
-    if msg.chat.id not in TARGET_CHAT_IDS:
+    if not msg.forward_from_chat:
         return
 
     log.warning(f'Send message to Chat(id={msg.chat.id}, title="{msg.chat.title}")')
@@ -47,6 +46,4 @@ async def send_comment(_, msg: types.Message):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.WARNING)
 
-    loop = asyncio.get_event_loop()
-    loop.create_task(on_startup())
-    bot.run()
+    bot.start()
